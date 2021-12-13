a5d12df7f494eab9e7cefb5a51a69cd6237447c5
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-13 17:15:56 +0500

    add entity

--- app/src/main/java/com/bakigoal/sleeptracker/database/SleepNight.kt
+++ app/src/main/java/com/bakigoal/sleeptracker/database/SleepNight.kt
@@ -1,1 +1,21 @@
 package com.bakigoal.sleeptracker.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight(
+
+    @PrimaryKey(autoGenerate = true)
+    val nightId: Long = 0L,
+
+    @ColumnInfo(name = "start_time_milli")
+    val startTimeMilli: Long = System.currentTimeMillis(),
+
+    @ColumnInfo(name = "end_time_milli")
+    val endTimeMilli: Long = startTimeMilli,
+
+    @ColumnInfo(name = "quality_rating")
+    val sleepQuality: Int = -1
+)
