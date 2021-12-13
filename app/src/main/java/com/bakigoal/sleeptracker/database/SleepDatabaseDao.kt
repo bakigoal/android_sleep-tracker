48b2a65acc815ec1c62838c3ef048d65f0bdc282
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-13 17:16:12 +0500

    add dao

--- app/src/main/java/com/bakigoal/sleeptracker/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/bakigoal/sleeptracker/database/SleepDatabaseDao.kt
@@ -1,3 +1,29 @@
 package com.bakigoal.sleeptracker.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface SleepDatabaseDao {
+
+    @Insert
+    fun insert(night: SleepNight)
+
+    @Update
+    fun update(night: SleepNight)
+
+    @Delete
+    fun delete(night: SleepNight)
+
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    @Query("SELECT * FROM daily_sleep_quality_table WHERE nightId = :nightId")
+    fun get(nightId: Long): SleepNight
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+}
