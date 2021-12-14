ff44b52e1b5445b09652ebd7064040b06c678559
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-14 18:52:19 +0500

    use binding adapter and kotlin extension

---
+++ app/src/main/java/com/bakigoal/sleeptracker/sleeptracker/recyclerview/BindingUtils.kt
@@ -0,0 +1,40 @@
+package com.bakigoal.sleeptracker.sleeptracker.recyclerview
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.bakigoal.sleeptracker.R
+import com.bakigoal.sleeptracker.convertDurationToString
+import com.bakigoal.sleeptracker.convertNumericQualityToString
+import com.bakigoal.sleeptracker.database.SleepNight
+
+@BindingAdapter("sleepDurationFormatted")
+fun TextView.setSleepDurationFormatted(item: SleepNight?) {
+    item?.let {
+        text = convertDurationToString(item.startTimeMilli, item.endTimeMilli, context.resources)
+    }
+}
+
+@BindingAdapter("sleepQualityString")
+fun TextView.setSleepQualityString(item: SleepNight?) {
+    item?.let {
+        text = convertNumericQualityToString(item.sleepQuality, context.resources)
+    }
+}
+
+@BindingAdapter("sleepQualityImage")
+fun ImageView.setSleepQualityImage(item: SleepNight?) {
+    item?.let {
+        setImageResource(
+            when (item.sleepQuality) {
+                0 -> R.drawable.ic_sleep_0
+                1 -> R.drawable.ic_sleep_1
+                2 -> R.drawable.ic_sleep_2
+                3 -> R.drawable.ic_sleep_3
+                4 -> R.drawable.ic_sleep_4
+                5 -> R.drawable.ic_sleep_5
+                else -> R.drawable.ic_sleep_active
+            }
+        )
+    }
+}
--- app/src/main/java/com/bakigoal/sleeptracker/sleeptracker/recyclerview/ViewHolder.kt
+++ app/src/main/java/com/bakigoal/sleeptracker/sleeptracker/recyclerview/ViewHolder.kt
@@ -3,9 +3,6 @@ package com.bakigoal.sleeptracker.sleeptracker.recyclerview
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bakigoal.sleeptracker.R
-import com.bakigoal.sleeptracker.convertDurationToString
-import com.bakigoal.sleeptracker.convertNumericQualityToString
 import com.bakigoal.sleeptracker.database.SleepNight
 import com.bakigoal.sleeptracker.databinding.ListItemSleepNightBinding
 
@@ -13,20 +10,8 @@ class ViewHolder private constructor(val binding: ListItemSleepNightBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: SleepNight) {
-        val res = binding.root.resources
-        binding.sleepLength.text = convertDurationToString(item.startTimeMilli, item.endTimeMilli, res)
-        binding.qualityString.text = convertNumericQualityToString(item.sleepQuality, res)
-        binding.qualityImage.setImageResource(getImage(item.sleepQuality))
-    }
-
-    private fun getImage(sleepQuality: Int) = when (sleepQuality) {
-        0 -> R.drawable.ic_sleep_0
-        1 -> R.drawable.ic_sleep_1
-        2 -> R.drawable.ic_sleep_2
-        3 -> R.drawable.ic_sleep_3
-        4 -> R.drawable.ic_sleep_4
-        5 -> R.drawable.ic_sleep_5
-        else -> R.drawable.ic_sleep_active
+        binding.sleep = item
+        binding.executePendingBindings()
     }
 
     companion object {
