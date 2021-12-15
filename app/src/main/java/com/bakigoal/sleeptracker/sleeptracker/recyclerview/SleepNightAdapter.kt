e45e162d50d95186bc9cef10a820eaf1925e1236
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-15 11:41:45 +0500

    refactoring

--- app/src/main/java/com/bakigoal/sleeptracker/sleeptracker/recyclerview/SleepNightAdapter.kt
+++ app/src/main/java/com/bakigoal/sleeptracker/sleeptracker/recyclerview/SleepNightAdapter.kt
@@ -6,8 +6,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bakigoal.sleeptracker.database.SleepNight
 import com.bakigoal.sleeptracker.sleeptracker.recyclerview.util.SleepNightDiffCallback
 import com.bakigoal.sleeptracker.sleeptracker.recyclerview.util.SleepNightListener
-import com.bakigoal.sleeptracker.sleeptracker.recyclerview.viewholders.SleepNightViewHolder
 import com.bakigoal.sleeptracker.sleeptracker.recyclerview.viewholders.HeaderViewHolder
+import com.bakigoal.sleeptracker.sleeptracker.recyclerview.viewholders.SleepNightViewHolder
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -21,6 +21,13 @@ class SleepNightAdapter(private val clickListener: SleepNightListener) :
 
     private val adapterScope = CoroutineScope(Dispatchers.Default)
 
+    override fun getItemViewType(position: Int): Int {
+        return when (getItem(position)) {
+            is DataItem.Header -> ITEM_VIEW_TYPE_HEADER
+            is DataItem.SleepNightItem -> ITEM_VIEW_TYPE_ITEM
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
             ITEM_VIEW_TYPE_HEADER -> HeaderViewHolder.from(parent)
@@ -29,10 +36,12 @@ class SleepNightAdapter(private val clickListener: SleepNightListener) :
         }
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return when (getItem(position)) {
-            is DataItem.Header -> ITEM_VIEW_TYPE_HEADER
-            is DataItem.SleepNightItem -> ITEM_VIEW_TYPE_ITEM
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is SleepNightViewHolder -> {
+                val item = getItem(position) as DataItem.SleepNightItem
+                holder.bind(item.sleepNight, clickListener)
+            }
         }
     }
 
@@ -46,16 +55,6 @@ class SleepNightAdapter(private val clickListener: SleepNightListener) :
                 submitList(items)
             }
         }
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when (holder) {
-            is SleepNightViewHolder -> {
-                val item = getItem(position) as DataItem.SleepNightItem
-                holder.bind(item.sleepNight, clickListener)
-            }
-        }
-
     }
 
     sealed class DataItem {
