04a5612257d5c6d0f28eff355284a684e6bb56a9
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-16 18:46:26 +0500

    fix error

--- app/src/main/java/com/bakigoal/sleeptracker/sleeptracker/recyclerview/util/SleepNightDiffCallback.kt
+++ app/src/main/java/com/bakigoal/sleeptracker/sleeptracker/recyclerview/util/SleepNightDiffCallback.kt
@@ -1,13 +1,13 @@
 package com.bakigoal.sleeptracker.sleeptracker.recyclerview.util
 
 import androidx.recyclerview.widget.DiffUtil
-import com.bakigoal.sleeptracker.sleeptracker.recyclerview.DataItem
+import com.bakigoal.sleeptracker.sleeptracker.recyclerview.SleepNightAdapter
 
-class SleepNightDiffCallback : DiffUtil.ItemCallback<DataItem>() {
+class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNightAdapter.DataItem>() {
 
-    override fun areItemsTheSame(oldItem: DataItem, newItem: DataItem) =
+    override fun areItemsTheSame(oldItem: SleepNightAdapter.DataItem, newItem: SleepNightAdapter.DataItem) =
         oldItem.id == newItem.id
 
-    override fun areContentsTheSame(oldItem: DataItem, newItem: DataItem) =
+    override fun areContentsTheSame(oldItem: SleepNightAdapter.DataItem, newItem: SleepNightAdapter.DataItem) =
         oldItem == newItem
 }
